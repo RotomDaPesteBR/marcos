5a81dfa7a34e397b06110d335350bca7cb0293c1
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2023-06-06 13:01:53 -0300

    1.0

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 60vh;
   color: ${({ theme }) => theme.colors.text};
   display: flex;
   flex-direction: column;
@@ -27,9 +27,10 @@ export const Medias = styled.div`
 `;
 
 export const MediasImg = styled.img`
-  height: 5vw;
-  width: 5vw;
+  height: 4vw;
+  width: 4vw;
   margin: 1.25vw;
+  margin-bottom: 0;
   -webkit-user-select: none;
   -ms-user-select: none;
   user-select: none;
@@ -47,7 +48,8 @@ export const Info = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 1.5vw;
+  font-size: 1.25vw;
+  padding-bottom: 2.5vw;
 
   @media (max-width: 500px) {
     font-size: 2.5vw;
@@ -74,7 +76,7 @@ export const FooterText = styled.div`
   justify-content: end;
   align-items: center;
   position: relative;
-  font-size: 2vw;
+  font-size: 1vw;
   bottom: 3.5vw;
 
   @media (max-width: 500px) {
@@ -84,7 +86,7 @@ export const FooterText = styled.div`
 
 export const FooterImg = styled.img`
   height: auto;
-  width: 70vw;
+  width: 50vw;
   position: absolute;
   z-index: -1;
   bottom: 0;
